e1cdbece4fb8c1f64099f551602adb31b5f554a2
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-03-12 13:15:23 -0700

    Mini Capstone additions: Edit and Delete actions on webpage

--- app/controllers/products_controller_controller.rb
+++ app/controllers/products_controller_controller.rb
@@ -1,16 +1,52 @@
 class ProductsControllerController < ApplicationController
-	def view_products_method
-		@products = "Some products"
-		render 'products.html.erb'
+	def show
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
 	end
 
-	def new_product
+	def index
+		@products = Product.all
 	end
 
-	def create_product
+	def new #no code needed
+	end
+
+	def create
 		input_name = params[:product_name]
-		input_price = params[:product_price]
+		input_price = params[:product_price].to_i
 		input_description = params[:product_description]
 		@product_add = Product.create(name: input_name, price: input_price, description: input_description)
 	end
+
+	def edit
+		recipe_id = params[:id]
+		@product = Product.find_by(id: recipe_id)
+	end
+
+	def update
+	#	input_name = params[:product_name]
+	#	input_price = params[:product_price]
+	#	input_description = params[:product_description]
+
+
+		@product = Product.find_by(id: params[:id])
+		@product.name = params[:product_name]
+		@product.price = params[:product_price]
+		@product.description = params[:product_description]
+		@product.save
+
+		redirect_to "/products/#{@product.id}"
+	end
+
+	def destroy
+		@product = Product.find_by(id: params[:id])
+		@product.destroy
+		redirect_to "/products"
+	end
+
+	#def create_product
+	#	input_name = params[:product_name]
+	#	input_description = params[:product_description]
+	#	@product_add = Product.create(name: input_name, price: input_price, description: input_description)
+	#end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/products" => "products_controller#view_products_method"
+
+
   get "/new_product" => "products_controller#new_product"
   post "/new_product" => "products_controller#create_product"
+# Restful routes
+  get "/products" => "products_controller#index"
+  get "/products/new" => "products_controller#new"
+  # must get to products below, no same name complications
+  post "/products" => "products_controller#create"
+  #wildcards will take any parameter, very helpful to put at end of routes
+  get "/products/:id" => "products_controller#show"
 
-end
+  #Edit and update
+  get "/products/:id/edit" => "products_controller#edit"
+  patch "/products/:id" => "products_controller#update"
+
+  delete "/products/:id" => "products_controller#destroy"
+
+
+
+
+end 
